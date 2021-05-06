079ef48c36830b3ce8cd1834922120da1210abda
Gisele Blair <giseleblair@icloud.com>
2021-05-05 17:52:25 -0700

    Added styling
    added bootstrap framework styling and some jquery navigation scrolling

--- routing/wildcards/scripts/main.js
+++ routing/wildcards/scripts/main.js
@@ -1,4 +1,38 @@
-// You can add custom JavaScript here
-// or create additional files.
-//
-// Learn more here: https://developer.zesty.io/docs/code-editor/javascript-files/
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using anime.js
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top - 100,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").on('click', function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 100,
+    });
+
+})(jQuery); // End of use strict
