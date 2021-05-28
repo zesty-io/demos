29c781665dc3ccc01ad387e61a54ea457b0c52f8
David Naimi <d88naimi@gmail.com>
2021-05-28 13:22:56 -0700

    formatting html

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -15,44 +15,38 @@ export default function Home(props) {
 
       <main className="container">
         <h1 >
-          Base homepage- doc links to come
+            Base homepage- doc links to come
         </h1>
         <h4>Demos</h4>
         <ul>
             <li><a href="search-example">Search Example</a> with ZFlix movies</li>
         </ul>
-        <div >
-      <h1>ZFlix with Nextjs</h1>
-      <h3>Fetching data</h3>
-
-      <p><a target="_blank" href="https://nextjs.org/learn/basics/data-fetching/getstaticprops-details"><strong>getStaticPros</strong></a> Next.js will run this function at build time. Whatever your return as props will be passed into the exported page.</p>
-
-      <p> <a target="_blank" href="https://nextjs.org/learn/basics/data-fetching/request-time"><strong>getServerSideProps</strong></a> This will be called at runtime during every request. So unlike getStaticProps, you will have the runtime data like query params, HTTP headers, and the req and res objects from API handlers.</p>
-
-      <p> <a target="_blank" href="https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation"><strong>getStaticPaths</strong></a> This will be called at runtime during every request. So unlike getStaticProps, you will have the runtime data like query params, HTTP headers, and the req and res objects from API handlers.</p>
-
-      <h4>Here we are fetching with nextjs `getServerSideProps` to Zflix `http://4q6k83l9-dev.preview.zesty.io/-/gql/movies_and_shows.json` API and mapping through to display image, title, and description</h4>
-
-      <div className="card-container">
-        {props.data.map((data, index) => (
-          <div className="card" >
-              <a target="_blank" href={data.poster}>
-                <div className='card-content' >
-                  <h1>{data.title}</h1>
-                  <img
-                      height="auto"
-                      width="200px"
-                      max-width="auto"
-                      src={data.film_logo}
-                    />
-                    <p> {data.meta_description}</p>
-                </div>
-              </a>
-          </div>
-        ))}
-      </div>
-    </div>
-
+        <div className="container-grid">
+            <h1>ZFlix with Nextjs</h1>
+            <h3>Fetching data</h3>
+            <p><a target="_blank" href="https://nextjs.org/learn/basics/data-fetching/getstaticprops-details"><strong>getStaticPros</strong></a> Next.js will run this function at build time. Whatever your return as props will be passed into the exported page.</p>
+            <p> <a target="_blank" href="https://nextjs.org/learn/basics/data-fetching/request-time"><strong>getServerSideProps</strong></a> This will be called at runtime during every request. So unlike getStaticProps, you will have the runtime data like query params, HTTP headers, and the req and res objects from API handlers.</p>
+            <p> <a target="_blank" href="https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation"><strong>getStaticPaths</strong></a> This will be called at runtime during every request. So unlike getStaticProps, you will have the runtime data like query params, HTTP headers, and the req and res objects from API handlers.</p>
+            <h4>Here we are fetching with nextjs `getServerSideProps` to Zflix `http://4q6k83l9-dev.preview.zesty.io/-/gql/movies_and_shows.json` API and mapping through to display image, title, and description</h4>
+            <div className="card-container">
+              {props.data.map((data, index) => (
+              <div className="card" >
+                  <a target="_blank" href={data.poster}>
+                    <div className='card-content' >
+                        <h1>{data.title}</h1>
+                        <img
+                          height="auto"
+                          width="200px"
+                          max-width="auto"
+                          src={data.film_logo}
+                          />
+                        <p> {data.meta_description}</p>
+                    </div>
+                  </a>
+              </div>
+              ))}
+            </div>
+        </div>
       </main>
       <style jsx>{`
             a{
@@ -60,12 +54,16 @@ export default function Home(props) {
               color: #000;
             }
 
+
           .container{
             width: 100%;
             background-color: #f2f4fb;
             display: flex;
             flex-direction: column;
             padding: 80px 75px;
+          }
+          .container-grid{
+            align-self: center;
           }
           .card-container {
             grid-template-columns: repeat(4, 400px);
