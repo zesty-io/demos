a518b7f0145ad5115921f78dd4bbbb452983dff2
David Naimi <d88naimi@gmail.com>
2021-06-01 15:49:00 -0700

    added developerHint for blogs

--- nextjs/pages/blog/index.js
+++ nextjs/pages/blog/index.js
@@ -1,12 +1,10 @@
+import React from 'react';
+import DeveloperHint from '../../components/DeveloperHint';
+import Layout from '../../components/Layout/';
+import { getPosts } from '../../api/posts';
+import Post from '../../components/Post';
 
-import React from 'react'
-import DeveloperHint from '../../components/DeveloperHint'
-import Layout from '../../components/Layout/'
-import { getPosts } from '../../api/posts'
-import Post from '../../components/Post'
-
-
-const IndexPage = ({  posts }) => (
+const IndexPage = ({ posts }) => (
   <Layout>
     <ul>
       {console.log(posts)}
@@ -17,13 +15,26 @@ const IndexPage = ({  posts }) => (
     <DeveloperHint>
       <p>
         <strong>Zesty.io Hint:</strong>
-
+        This NextJS example uses{' '}
+        <a
+          target='_blank'
+          href='https://4q6k83l9-dev.preview.zesty.io/my/custom/endpoint.json?limit=5'
+        >
+          <strong>Custom API Endpoint</strong>
+        </a>{' '}
+        to load the data, and uses a javascript array map to power the search.
+        Data is preloaded using a NextJS function{' '}
+        <a
+          title='getServerSideProps'
+          target='_blank'
+          href='https://nextjs.org/learn/basics/data-fetching/request-time'
+        >
+          <strong>getServerSideProps</strong>
+        </a>{' '}
       </p>
     </DeveloperHint>
   </Layout>
 );
-
-
 
 export async function getServerSideProps() {
   const res = await getPosts();
@@ -36,4 +47,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default IndexPage
+export default IndexPage;
