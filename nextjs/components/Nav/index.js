37297ff2374d4cd9cffd2a4a8e52ed8f18ea18e0
Stuart Runyan <shrunyan@gmail.com>
2021-07-22 17:39:30 -0700

    WIP: demo of nextjs and react-autolayout

--- nextjs/components/Nav/index.js
+++ nextjs/components/Nav/index.js
@@ -1,30 +1,33 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import styles from './Nav.module.css';
+import styles from "./Nav.module.css";
 
 const Nav = () => (
   <div className={styles.Nav}>
     <a
-      target='_blank'
-      href='https://github.com/zesty-io/demos/tree/main/nextjs'
+      target="_blank"
+      href="https://github.com/zesty-io/demos/tree/main/nextjs"
     >
       Nextjs Demo
     </a>
-    <Link href='/'>
+    <Link href="/">
       <a>Home (ServerSideProps) </a>
-    </Link>{' '}
-    <Link href='/search-example'>
+    </Link>{" "}
+    <Link href="/search-example">
       <a>Search Example (StaticProps)</a>
     </Link>
-    <Link href='/flix'>
+    <Link href="/flix">
       <a>Flix (StaticProps Dynamic Routes) </a>
     </Link>
-    <Link href='/blog'>
+    <Link href="/blog">
       <a>Blog (ServerProps Dynamic Routes)</a>
     </Link>
+    <Link href="/autolayout">
+      <a>AutoLayout</a>
+    </Link>
     <img
-      src='https://brand.zesty.io/zesty-io-logo-horizontal-light.png'
-      alt='Zesty.io'
+      src="https://brand.zesty.io/zesty-io-logo-horizontal-light.png"
+      alt="Zesty.io"
     />
   </div>
 );
---
+++ nextjs/pages/autolayout/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout/";
+
+import { AutoLayout } from "@zesty-io/react-autolayout";
+
+const IndexPage = ({ posts }) => (
+  <Layout>
+    <AutoLayout instanceZUID="parsleydev-dev" modelZUID="6-ddf428-gd0xxn" />
+  </Layout>
+);
+
+export default IndexPage;
